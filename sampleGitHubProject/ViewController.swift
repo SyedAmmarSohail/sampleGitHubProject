a8fdee4f447de49d2f8803ddcb520bbfda63863f
Syed Ammar <syedammar@Admins-Mac-mini.local>
2019-05-29 09:57:50 +0500

    first commit

---
+++ sampleGitHubProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sampleGitHubProject
+//
+//  Created by Syed Ammar on 29/05/2019.
+//  Copyright © 2019 Syed Ammar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
